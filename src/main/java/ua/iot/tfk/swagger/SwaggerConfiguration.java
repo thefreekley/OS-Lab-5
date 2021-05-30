885515c82f2514ffe8accf12353ae6f32bd7ee2a
Andrey Matviichuk <thefreekley@gmail.com>
2021-05-30 19:14:42 +0300

    fix swagger v2

--- src/main/java/ua/iot/tfk/swagger/SwaggerConfiguration.java
+++ src/main/java/ua/iot/tfk/swagger/SwaggerConfiguration.java
@@ -1,12 +1,7 @@
 package ua.iot.tfk.swagger;
 
-import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.hateoas.client.LinkDiscoverer;
-import org.springframework.hateoas.client.LinkDiscoverers;
-import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
-import org.springframework.plugin.core.SimplePluginRegistry;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -14,8 +9,6 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @EnableSwagger2
 @Configuration
